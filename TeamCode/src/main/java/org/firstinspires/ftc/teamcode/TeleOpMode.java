43148598bba5efbb7b32b9edbd742a77c35924b5
3th3rn3t <prathampmehta@gmail.com>
2022-01-09 17:24:32 -0800

    Test Classes Added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -3,19 +3,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "Concept: Main", group = "Concept")
+@TeleOp (name = "TeleOp", group = "Concept")
 //@Disabled
 public class TeleOpMode extends LinearOpMode {
 
-    SlidesPID slides;
-    FieldOrientedDrive driver;
+    Slides slides;
+    FieldOrientatedDrive driver;
     Trajectories trajectories;
     Intake intake;
     CarouselMechanism carouselMechanism;
 
     @Override
     public void runOpMode() {
-        driver = new FieldOrientedDrive(gamepad1);
+        driver = new FieldOrientatedDrive(gamepad1);
         intake = new Intake(gamepad1, 1);
         carouselMechanism = new CarouselMechanism(gamepad1);
 
@@ -30,6 +30,7 @@ public class TeleOpMode extends LinearOpMode {
 
             carouselMechanism.control();
 
+            /*
             Trajectories.Action[] actions = new Trajectories.Action[]{
                     () -> Trajectories.moveForward(100),
                     () -> Trajectories.moveForward(100),
@@ -39,6 +40,7 @@ public class TeleOpMode extends LinearOpMode {
             for (Trajectories.Action action : actions) {
                 action.move();
             }
+            */
 
 
             telemetry.addData("Vector X", driver.vector.getX());
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/CarouselMechanismTest.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp (name = "Concept: CarouselMechanism", group = "Test")
+public class CarouselMechanismTest extends LinearOpMode {
+    CRServo carouselServo1;
+    CRServo carouselServo2;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        carouselServo1 = hardwareMap.crservo.get("carouselServo1");
+        carouselServo2 = hardwareMap.crservo.get("carouselServo2");
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            if (gamepad1.x) {
+                carouselServo1.setPower(1);
+                carouselServo2.setPower(-1);
+            }
+            else {
+                carouselServo1.setPower(0);
+                carouselServo2.setPower(0);
+            }
+        }
+    }
+}
