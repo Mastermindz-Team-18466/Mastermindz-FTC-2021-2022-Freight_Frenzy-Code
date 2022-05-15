a18c3afae8b5160fa6c68698a9fdf1c241793035
Aadi <aadi@mastermindz.net>
2022-05-14 17:16:02 -0700

    enum error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Outtake2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Outtake2.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -7,12 +8,15 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import java.util.Arrays;
 import java.util.List;
 
-
+@Config
 public class Outtake2 {
     public static double kp = 0.1;
     public static double bottom = 0;
     public static double mid = -40;
     public static double top = -60;
+    private double targetPos;
+    public final Claw claw;
+    public final V4B v4b;
 
     public enum liftPos {
         BOTTOM,
@@ -21,26 +25,36 @@ public class Outtake2 {
         PEAK
     }
 
-    public enum outtakePos {
-        BOTTOM_CLOSE,
-        BOTTOM_OPEN,
-        MID,
-        TOP,
-        PEAK,
+    public enum outtakePosEnum {
+        CLOSE,
+        OPEN,
+        SHARED,
         TSE
     }
 
+    public enum outtakeInstructionsEnum {
+        CLAW_OPEN,
+        CLAW_CLOSED,
+        V4B_OPEN,
+        V4B_CLOSED,
+        CLAW_WAIT_OPEN
+    }
+
     private liftPos targetLiftPos;
+    private outtakePosEnum outtakePos;
+    private outtakeInstructionsEnum outtakeInstructions;
     private DcMotor left_linear_slide, right_linear_slide;
 
     HardwareMap hardwareMap;
 
-    public outtakePos currentPos = outtakePos.BOTTOM_CLOSE;
-    private long start_time = System.currentTimeMillis();
-    private long prevClick = System.currentTimeMillis();
+    private long startTime = System.currentTimeMillis();
+    private long prevAction = System.currentTimeMillis();
+    public long currentTime = 0;
 
-    public Outtake2(HardwareMap hardwareMap) {
+    public Outtake2(HardwareMap hardwareMap, Claw claw, V4B v4b) {
         targetLiftPos = liftPos.BOTTOM;
+        this.claw = claw;
+        this.v4b = v4b;
 
         //Left Linear Slide
         left_linear_slide = hardwareMap.get(DcMotor.class, "leftLinear_slide");
@@ -55,8 +69,10 @@ public class Outtake2 {
 
     public void update() {
 
-        double targetPos;
-        switch(targetLiftPos){
+        switch (targetLiftPos) {
+            case BOTTOM:
+                targetPos = bottom;
+                break;
             case MID:
                 targetPos = mid;
                 break;
@@ -64,27 +80,71 @@ public class Outtake2 {
                 targetPos = top;
                 break;
             default:
-                targetPos = bottom;
                 break;
         }
 
-        double averagePos = (getCurrentPosition().get(0) + (getCurrentPosition().get(1) * -1)) / 2 / 19.5;
+        double averagePos = getPosition();
         double p = kp * (targetPos - averagePos);
+
 
         setLiftMotorPower(p);
 
-        if (-(Math.abs(left_linear_slide.getCurrentPosition())) == targetPos || -(Math.abs(right_linear_slide.getCurrentPosition())) == targetPos){
-            stop();
+        switch (outtakePos) {
+            case OPEN:
+                switch (outtakeInstructions) {
+                    case CLAW_CLOSED:
+                        claw.control(Claw.State.CLOSE);
+                        prevAction = System.currentTimeMillis();
+                        outtakeInstructions = outtakeInstructions.V4B_OPEN;
+                        break;
+                    case V4B_OPEN:
+                        if (System.currentTimeMillis() - prevAction > 250) {
+                            v4b.control(V4B.State.OPEN);
+                        }
+                        break;
+                }
+                break;
+            case CLOSE:
+                switch (outtakeInstructions) {
+                    case CLAW_OPEN:
+                        claw.control(Claw.State.OPEN);
+                        prevAction = System.currentTimeMillis();
+                        outtakeInstructions = outtakeInstructions.CLAW_CLOSED;
+                        break;
+                    case CLAW_CLOSED:
+                        if (System.currentTimeMillis() - prevAction > 300) {
+                            claw.control(Claw.State.CLOSE);
+                            v4b.control(V4B.State.CLOSE);
+                            prevAction = System.currentTimeMillis();
+                            outtakeInstructions = outtakeInstructions.CLAW_WAIT_OPEN;
+                        }
+                        break;
+                    case CLAW_WAIT_OPEN:
+                        if (System.currentTimeMillis() - prevAction > 1000) {
+                            claw.control(Claw.State.OPEN);
+                        }
+                        break;
+                }
+                break;
         }
 
-        if (p > 0){
-            p = 0;
-        }
 
     }
 
     public void setTargetLiftPos(liftPos pos) {
         targetLiftPos = pos;
+    }
+
+    public void setOuttakePos(outtakePosEnum pos) {
+        outtakePos = pos;
+    }
+
+    public void setOuttakeInstructions(outtakeInstructionsEnum instruction) {
+        outtakeInstructions = instruction;
+    }
+
+    public double getPosition() {
+        return (getCurrentPosition().get(0) + (getCurrentPosition().get(1) * -1)) / 2 / 19.5;
     }
 
     public void setLiftMotorPower(double power) {
@@ -94,11 +154,6 @@ public class Outtake2 {
 
     public List<Integer> getCurrentPosition() {
         return Arrays.asList(left_linear_slide.getCurrentPosition(), right_linear_slide.getCurrentPosition());
-    }
-
-    public void stop() {
-        left_linear_slide.setPower(0);
-        right_linear_slide.setPower(0);
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMode.java
@@ -3,9 +3,7 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-import org.checkerframework.checker.units.qual.C;
 import org.firstinspires.ftc.teamcode.TeleOpFieldCentric;
 import org.firstinspires.ftc.teamcode.auto.Trajectories;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
@@ -32,7 +30,7 @@ public class TeleOpMode extends LinearOpMode {
         claw = new Claw(gamepad1, hardwareMap);
         carouselMechanism = new CarouselMechanism(gamepad1, hardwareMap);
         outtake = new Outtake(new SlidesTeleOp(gamepad1, hardwareMap), new V4B(gamepad1, hardwareMap), new Claw(gamepad1, hardwareMap), gamepad1);
-        outtake2 = new Outtake2(hardwareMap);
+        outtake2 = new Outtake2(hardwareMap, new Claw(gamepad1, hardwareMap), new V4B(gamepad1, hardwareMap));
 
         outtake.set(Outtake.Position.BACK);
 
@@ -41,10 +39,14 @@ public class TeleOpMode extends LinearOpMode {
         while (opModeIsActive()) {
             if (gamepad1.b) {
                 outtake2.setTargetLiftPos(Outtake2.liftPos.BOTTOM);
+                outtake2.setOuttakePos(Outtake2.outtakePosEnum.CLOSE);
+                outtake2.setOuttakeInstructions(Outtake2.outtakeInstructionsEnum.CLAW_OPEN);
             } else if (gamepad1.dpad_down) {
                 outtake.set(Outtake.Position.BOTTOM_FORWARD);
             } else if (gamepad1.dpad_up) {
                 outtake2.setTargetLiftPos(Outtake2.liftPos.TOP);
+                outtake2.setOuttakePos(Outtake2.outtakePosEnum.OPEN);
+                outtake2.setOuttakeInstructions(Outtake2.outtakeInstructionsEnum.CLAW_CLOSED);
             }
 
             if (gamepad1.left_bumper) {
@@ -77,12 +79,13 @@ public class TeleOpMode extends LinearOpMode {
             }
 
             telemetry.update();
+            outtake2.update();
         }
 
         telemetry.addData(">", "Done");
         telemetry.update();
 
-        outtake2.update();
+
 
     }
 }
